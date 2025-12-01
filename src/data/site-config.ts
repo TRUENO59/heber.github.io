f53a7b6c31c6104320312b27ef1aa2598d784973
TRUENO59 <hmanrique@hotmail.com>
2025-12-01 19:45:26 -0400

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -98,8 +98,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hola Bienvenido a mi pagina Web!',
+        text: "Soy el Dr. Jean Acevedo especializado en Cirugia Plastica Reconstructiva, Ofrecemos los avances más recientes y procedimientos ambulatorios innovadores en cirugía estética, garantizando resultados satisfactorios, seguros y confiables. Nuestra Unidad está equipada con recursos modernos que permiten una práctica cómoda y de alto nivel, gracias a instalaciones óptimas y tecnología de vanguardia. Además, el Dr. Acevedo y su equipo brindan un acompañamiento personalizado durante todo el proceso preoperatorio, postoperatorio y en los cuidados necesarios para cada tratamiento, asegurando atención cercana y de calidad para cada paciente. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> o sigame en <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/acevedo.jpg',
             alt: 'A person sitting at a desk in front of a computer'
